56e57475c3b8fd2622ce8fc4e18b6442c3370cf0
Tyrolyean <tyrolyean@tyrolyean.net>
2018-08-20 00:08:18 +0200

    Version bump
    Signed-off-by: Tyrolyean <tyrolyean@tyrolyean.net>

--- include/data.h
+++ include/data.h
@@ -26,8 +26,8 @@
  *
  */
 #define VERSION_MAJOR           0
-#define VERSION_MINOR           3
-#define VERSION_RELEASE         3
+#define VERSION_MINOR           4
+#define VERSION_RELEASE         0
 
 #define SHUTDOWN_DELAY 5
 
